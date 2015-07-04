8e49b8e318638866684285a54b293b6f33fbb497
YuanYu Lin <yuanyu.lin@gmail.com>
2015-07-04 19:56:20 +0800

    add new functions.

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ unsigned long parse_paramegers(int argc, char** argv, char* json_file, char* jso
     unsigned long func = 0x0;
     int cmd_opt = 0;
     while(1) {
-        cmd_opt = getopt(argc, argv, "f:k:slv");
+        cmd_opt = getopt(argc, argv, "f:k:alvt");
 
         if(cmd_opt == -1) {
             return func;
@@ -24,14 +24,20 @@ unsigned long parse_paramegers(int argc, char** argv, char* json_file, char* jso
         case 'f': // json file
             strcpy(json_file, optarg);
         break;
-        case 's': // show all json
+        case 'a': // show all json
             func |= SHOW_JSON;
         break;
         case 'l': // show json array len
             func |= SHOW_ARRAY_LEN;
         break;
+        caes 's': // show json array string
+            func |= SHOW_ARRAY_STRING;
+        break;
         case 'v': // show json value
             func |= SHOW_VALUE;
+        break;
+        case 't': // show json type
+            func |= SHOW_TYPE;
         break;
         }
     }
@@ -43,11 +49,13 @@ static void show_usage()
 {
     printf("-f: input json file\n");
     printf("-k: json key string\n");
-    printf("-s: show all elements of file\n");
+    printf("-a: show all elements of file\n");
+    printf("-v: show value of element\n");
+    printf("-t: show type of element\n");
     printf("-l: show length of array element\n");
-    printf("-v: show vale of element\n");
+    printf("-s: show array strings\n");
     printf("example : \n");
-    printf("    utils -f vmcfg.json -s\n");
+    printf("    utils -f vmcfg.json -a\n");
     printf("    utils -f vmcfg.json -k type -v\n");
 }
 
@@ -58,6 +66,8 @@ int main(int argc, char** argv)
     struct query_json_obj_t query_json_obj;
     char json_file[STR_LEN];
     unsigned long func = 0x0;
+    int array_len = 0;
+    int isError = 0;
 
     if(argc < 2) {
         show_usage();
@@ -72,8 +82,48 @@ int main(int argc, char** argv)
     case SHOW_JSON:
         show_json(json_obj);
     break;
+    case SHOW_TYPE:
+        get_json_obj_by_query_obj(&query_json_obj, json_obj);
+        switch(query_json_obj.type){
+        case json_type_null:
+            printf("%s", "NULL");
+        break;
+        case json_type_boolean:
+            printf("%s", "BOOLEAN");
+        break;
+        case json_type_double:
+            printf("%s", "DOUBLE");
+        break;
+        case json_type_int:
+            printf("%s", "INTEGER");
+        break;
+        case json_type_string:
+            printf("%s", "STRING");
+        break;
+        case json_type_object:
+            printf("%s", "OBJECT");
+        break;
+        case json_type_array:
+            printf("%s", "ARRAY");
+        break;
+        default:
+            isError = 1;
+        break;
+    break;
     case SHOW_ARRAY_LEN:
-//        show_json_array_len(json_key, json_obj);
+        get_json_obj_by_query_obj(&query_json_obj, json_obj);
+        isError = 1;
+        if(query_json_obj.type == json_type_array) {
+            printf("%d", json_object_array_length(query_json_obj.obj));
+            isError = 0;
+        }
+    break;
+    case SHOW_ARRAY_STRING:
+        get_json_obj_by_query_obj(&query_json_obj, json_obj);
+        isError = 1;
+        if(query_json_obj.type == json_type_array) {
+            isError = 0;
+        }
     break;
     case SHOW_VALUE:
         get_json_obj_by_query_obj(&query_json_obj, json_obj);
@@ -90,21 +140,21 @@ int main(int argc, char** argv)
         case json_type_int:
             printf("%d", json_object_get_int(query_json_obj.obj));
         break;
-        case json_type_object:
-            printf("error");
-        break;
-        case json_type_array:
-            printf("error");
-        break;
         case json_type_string:
             printf("%s", json_object_get_string(query_json_obj.obj));
         break;
+        case json_type_object:
+        case json_type_array:
         default:
-            printf("error");
+            isError = 1;
         break;
         }
     break;
     }
+
+    if(isError)
+        printf("%s", ERROR_STRING);
+
     return 0;
 }
 
--- main.h
+++ main.h
@@ -1,10 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define ERROR_STRING	"ERROR_RETURN_VALUE"
+
 enum{
-    SHOW_JSON		= 0x01,
-    SHOW_ARRAY_LEN	= 0x02,
-    SHOW_VALUE		= 0x04,
+    SHOW_JSON		= (1<<0),
+    SHOW_ARRAY_LEN	= (1<<1),
+    SHOW_VALUE		= (1<<2),
+    SHOW_TYPE		= (1<<3),
+    SHOW_ARRAY_STRING	= (1<<4),
 };
 
 #endif
